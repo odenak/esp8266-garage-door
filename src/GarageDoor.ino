e347b0fce3faef9173f0c398e7cd7ec418b5a387
John Li <johnli@kanedo.com>
2017-05-03 17:44:06 -0500

    include Arduino.h in GarageDoor.ino

--- src/GarageDoor.ino
+++ src/GarageDoor.ino
@@ -1,3 +1,4 @@
+#include <Arduino.h>
 #include <ESP8266WiFi.h>            // ESP8266 Core WiFi Library (you most likely already have this in your sketch)
 #include <DNSServer.h>              // Local DNS Server used for redirecting all requests to the configuration portal
 #include <ESP8266WebServer.h>       // Local WebServer used to serve the configuration portal
